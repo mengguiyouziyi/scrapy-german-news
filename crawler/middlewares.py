ab64bf66b129f487ce246c84904eabd97791c1bb
sans-serif <noemailinside@gmail.com>
2016-02-25 17:00:36 +0100

    Added basic crawler for 'spiegel.de'

--- crawler/middlewares.py
+++ crawler/middlewares.py
@@ -11,20 +11,15 @@ class RotateUserAgentMiddleware(object):
     @classmethod
     def from_crawler(cls, crawler):
         user_agents = crawler.settings.get('USER_AGENT_CHOICES', [])
-
         if not user_agents:
             raise NotConfigured("USER_AGENT_CHOICES not set or empty")
-
-        o = cls(user_agents)
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-
-        return o
+        ret = cls(user_agents)
+        crawler.signals.connect(ret.spider_opened, signal=signals.spider_opened)
+        return ret
 
     def spider_opened(self, spider):
         self.enabled = getattr(spider, 'rotate_user_agent', self.enabled)
 
     def process_request(self, request, spider):
-        if not self.enabled or not self.user_agents:
-            return
-
-        request.headers['user-agent'] = choice(self.user_agents)
+        if self.enabled and self.user_agents:
+            request.headers['user-agent'] = choice(self.user_agents)
---
+++ crawler/spiders/spiegel.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http.request import Request
+
+from crawler.items import CrawlerItem
+from crawler.utils import get_first
+
+class SpiegelSpider(CrawlSpider):
+    name = 'spiegel'
+    rotate_user_agent = True
+    allowed_domains = ['www.spiegel.de']
+    start_urls = ['http://www.spiegel.de']
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=('(politik|wirtschaft)\/.*\/',),
+                deny=('forum','\.html')
+            ),
+            follow=True
+        ),
+        Rule(
+            LinkExtractor(
+                allow=('(politik|wirtschaft)\/.+\.html'),
+                deny=('forum')
+            ),
+            callback='parse_page',
+        ),
+    )
+
+    def parse_page(self, response):
+        item = CrawlerItem()
+        item['url'] = response.url.encode('utf-8')
+        item['visited'] = datetime.datetime.now().isoformat().encode('utf-8')
+        item['published'] = get_first(response.selector.xpath('//meta[@name="date"]/@content').extract())
+        item['title'] = get_first(response.selector.css('.headline').xpath('./text()').extract())
+        item['description'] = get_first(response.selector.xpath('//meta[@name="description"]/@content').extract())
+        item['text'] = "".join([s.strip().encode('utf-8') for s in response.selector.xpath('//div[@class="article-section clearfix"]/p/text()').extract()])
+        item['author'] = get_first(response.selector.xpath('//p[@class="author"]/a/text()').extract())
+        item['keywords'] = [s.encode('utf-8') for s in response.selector.xpath('//meta[@name="news_keywords"]/@content').extract()]
+        item['article_type'] = get_first(response.selector.xpath('//meta[@property="og:type"]/@content').extract())
+        return item
